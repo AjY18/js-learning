2f366a44028d2c3c89cdf0a047533d3f049601ad
AjY <139573572+AjY18@users.noreply.github.com>
2024-01-11 16:32:11 +0000

    add variable file

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const acc_no = 42315
+let email =" ajpatwal53@gmail.com"
+var password = "43545";
+city ="Mumbai";
+let name;
+let mob_no="";
+console.log(acc_no);
+
+/*acc_no=76546
+console.log(acc_no); // 
+we can not modifiy a const variable value  */
+console.table([email,password,city,name,mob_no])
+/* 
+prefer not ot use 'var'
+because of issue in block scope  and funtional scope
+*/ 
