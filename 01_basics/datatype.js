658b6b6f3d8b3d9eb76159f2167c24fbc734f092
AjY <139573572+AjY18@users.noreply.github.com>
2024-01-11 17:27:46 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3) // we are using in browser, not in node js 
+
+console.log(3+5);
+// code readability should be high
+console.log("ratan");
+
+//data type in js we have
+//number => range is 2 to power 53
+//bigint
+//string => " "
+//Boolean => true/false
+//null => standlone value
+//undefined => 
+//symbol => unique
+
+console.log(typeof undefined); // type = undefined
+console.log(typeof null); // type = object 
+// in js null type is obejct 
