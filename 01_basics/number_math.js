3414ede88cbfe773ca96e50b559bfb3de2bce195
AjY <139573572+AjY18@users.noreply.github.com>
2024-01-15 18:13:55 +0000

    number_math

--- 01_basics/number_math.js
+++ 01_basics/number_math.js
@@ -1,36 +1,36 @@
-// const score =100;
-// console.log(score);
+ const score =100;
+ console.log(score);
 
-// const one = new Number(4894)
-// console.log(one);
-// console.log(score.toString);
-// console.log(score.toString().length);
+const one = new Number(4894)
+console.log(one);
+console.log(score.toString);
+console.log(score.toString().length);
 
-// console.log(score.toFixed(2));  // a fixed help we can take a two extra zero after point:- 3.00 
+console.log(score.toFixed(2));  // a fixed help we can take a two extra zero after point:- 3.00 
 
-// const num =536.48993
-// console.log(num.toPrecision(5));
+const num =536.48993
+console.log(num.toPrecision(5));
 // // it's output is 536.49 precision give a priority after decimal point and  then its change the values
 
-// const two =3000000
-// console.log(two.toLocaleString('en-IN'));// output :- 30,00,000
+const two =3000000
+console.log(two.toLocaleString('en-IN'));// output :- 30,00,000
 //its help we can calculate a zeros in indian style using 'en-IN' 
 
 
 //+++++++++++++++ MATH  +++++++++++++++++
-// console.log(Math);
-// console.log(Math.abs(-32)); // output :- 32 remove minus form the value
+console.log(Math);
+console.log(Math.abs(-32)); // output :- 32 remove minus form the value
 
-// console.log(Math.round(23.49)); //output:- output is 23 it's take a round value which is before decimal point 
+console.log(Math.round(23.49)); //output:- output is 23 it's take a round value which is before decimal point 
 
-// console.log(Math.ceil(4.3));// output:- 5   ceil method help output always 5 if decimal value is present
+console.log(Math.ceil(4.3));// output:- 5   ceil method help output always 5 if decimal value is present
 
-// console.log(Math.floor(4.9));// output:- 4   floor method help output always 4 if decimal value is present
+console.log(Math.floor(4.9));// output:- 4   floor method help output always 4 if decimal value is present
 
-// console.log(Math.min(3,8,4,1,8,7)); //1
-// console.log(Math.max(3,8,4,1,8,7)); //8
+console.log(Math.min(3,8,4,1,8,7)); //1
+console.log(Math.max(3,8,4,1,8,7)); //8
 
-//console.log(Math.random());
+console.log(Math.random());
 //Math.random is always give a output between 1 and 0 
 
 console.log(Math.random()*10);
